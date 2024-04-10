7c48e73631fc45cafc806dc0b524218a0b11358f
Kathryn <kathrynpeters17@gmail.com>
2024-04-10 16:53:20 -0400

    Note

--- sj_main.py
+++ sj_main.py
@@ -7,6 +7,16 @@ def encoder(pw):
         encoded_pw += encoded_digit
     return encoded_pw
 
+#Decode function
+def decode(password):
+    decoded_pw = ""
+    for i in password:
+        decode_new = int(i) - 3
+        decoded_pw += str(decode_new)
+
+    return decoded_pw
+
+
 def menu():
     print("Menu\n"
           "-------------\n"
@@ -15,19 +25,21 @@ def menu():
           "3. Quit\n")
 
 def main():
-    menu()
-    try:
-        option = int(input("Please enter an option: "))
-        if option == 1:
-            password = input("Please enter your password to encode: ")
-            encoded_password = encoder(password)
-            print("Your password has been encoded and stored!")
-        elif option == 2:
-            pass
-        elif option == 3:
-            exit()
-    except ValueError:
-        print("Unrecognized menu selection!")
+    while True:
+        menu()
+        try:
+            option = int(input("Please enter an option: "))
+            if option == 1:
+                password = input("Please enter your password to encode: ")
+                encoded_password = encoder(password)
+                print("Your password has been encoded and stored!\n")
+            elif option == 2:
+                print(f"The encoded password is {encoder(password)}, and the original password is {decode(encoder(password))}.\n")
+
+            elif option == 3:
+                exit()
+        except ValueError:
+            print("Unrecognized menu selection!")
 
 
 if __name__ == "__main__":
